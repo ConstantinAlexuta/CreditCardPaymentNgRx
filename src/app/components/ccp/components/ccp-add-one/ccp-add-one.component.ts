77bb9ac687630ce8b8110dacd2a2d6c264ac57bc
ConstantinAlexuta <constantin.alexuta@gmail.com>
2021-01-26 22:00:01 +0200

    update work

--- src/app/components/ccp/components/ccp-add-one/ccp-add-one.component.ts
+++ src/app/components/ccp/components/ccp-add-one/ccp-add-one.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Ccp } from '../../model/ccp.model';
-import { CcpService } from '../../service/ccp.service';
 
 import { v4 as uuidv4 } from 'uuid';
 import { AppState } from 'src/store/reducers';
@@ -34,39 +33,110 @@ export class CcpAddOneComponent implements OnInit {
 
   public ccpFormGroup!: FormGroup;
 
-  constructor(
-    private store: Store<AppState>,
-    private ccpService: CcpService,
-    private activatedRoute: ActivatedRoute,
-    private router: Router
-  ) {
+  fixedlength_creditCardNumber: number = 16;
+  fixedlength_securityCodeCCV: number = 3;
+
+  minlength_cardHolder: number = 2;
+  minlength_expirationDate: number = 10;
+  minlength_amount: number = 1;
+
+  maxlength_cardHolder: number = 100;
+  maxlength_expirationDate: number = 10;
+  maxlength_amount: number = 20;
+
+  constructor(private store: Store<AppState>, private router: Router) {
     this.ccpFormGroup = new FormGroup({
-      creditCardNumber: new FormControl(
-        '',
-        // this.ccp.creditCardNumber,
-        [
-          // Validators.required,
-          // Validators.minLength(16),
-        ]
-      ),
+      creditCardNumber: new FormControl('', [
+        Validators.required,
+        Validators.minLength(this.fixedlength_creditCardNumber),
+        Validators.maxLength(this.fixedlength_creditCardNumber),
+      ]),
       cardHolder: new FormControl('', [
-        // Validators.required,
-        // Validators.minLength(2),
+        Validators.required,
+        Validators.minLength(this.minlength_cardHolder),
+        Validators.maxLength(this.maxlength_cardHolder),
       ]),
       expirationDate: new FormControl('', [
-        // Validators.required,
-        // Validators.minLength(10),
+        Validators.required,
+        Validators.minLength(this.minlength_expirationDate),
+        Validators.maxLength(this.maxlength_expirationDate),
       ]),
       amount: new FormControl('', [
-        // Validators.required,
-        // Validators.minLength(1),
+        Validators.required,
+        Validators.minLength(this.minlength_amount),
+        Validators.maxLength(this.maxlength_amount),
+        this.noSpacesValidator,
+        this.noCommaValidator,
+        this.onlyNumberValidator,
+        this.cannotStartWithDotValidator,
+        this.onlyTwoDecimalsValidator,
       ]),
       securityCodeCCV: new FormControl('', [
-        // Validators.required,
-        // Validators.minLength(3),
+        Validators.required,
+        Validators.minLength(this.fixedlength_securityCodeCCV),
+        Validators.maxLength(this.fixedlength_securityCodeCCV),
+        this.noSpacesValidator,
+        this.onlyDigitsValidator,
       ]),
-      // status: new FormControl('', Validators.required),
     });
+  }
+
+  noSpacesValidator(control: FormControl) {
+    return control.value.replace(' ', '').length == control.value.length
+      ? null
+      : { spacesArePresent: true };
+  }
+
+  onlyNumberValidator(control: FormControl) {
+    let isNumber =
+      control.value != null &&
+      control.value !== '' &&
+      !isNaN(Number(control.value.toString()));
+
+    return isNumber ? null : { isNotNumber: true };
+  }
+
+  onlyDigitsValidator(control: FormControl) {
+    let isNumber =
+      control.value != null &&
+      control.value !== '' &&
+      !isNaN(Number(control.value.toString()));
+
+    let noDotOrComma =
+      control.value.replace('.', '').replace(',', '').length ==
+      control.value.length;
+
+    return isNumber && noDotOrComma ? null : { areNotOnlyDigits: true };
+  }
+
+  noCommaValidator(control: FormControl) {
+    let noComma = control.value.replace(',', '').length == control.value.length;
+
+    return noComma ? null : { commaIsPresent: true };
+  }
+
+  cannotStartWithDotValidator(control: FormControl) {
+    let afterTrimStartWithDot = control.value.charAt(0) == '.';
+
+    return !afterTrimStartWithDot
+      ? null
+      : { dotIsFirstCharacterAfterTrim: true };
+  }
+
+  onlyTwoDecimalsValidator(control: FormControl) {
+    let numberOfDecimals = 0;
+
+    let haveDot = control.value.replace('.', '').length != control.value.length;
+
+    let indexOfDot = (control.value + '').indexOf('.', 0);
+
+    if (indexOfDot > 0) {
+      numberOfDecimals = control.value.length - indexOfDot - 1;
+    }
+
+    return !(haveDot && indexOfDot >= 1 && numberOfDecimals > 2)
+      ? null
+      : { moreThanTwoDecimals: true };
   }
 
   ngOnInit(): void {}
@@ -114,20 +184,31 @@ export class CcpAddOneComponent implements OnInit {
     this.showMessageAlreadySubmitted = false;
     this.showNewItemCreatedIndexMessage = false;
     this.firstAttemptToSaveWithValidFormWasDone = false;
+    this.clikOnSaveWithValidFormCount = 0;
   }
 
   validMessage: string = '';
+
+  clikOnSaveWithValidFormCount: number = 0;
+
+  onSave() {
+    if (this.ccpFormGroup.valid) {
+      this.clikOnSaveWithValidFormCount++;
+    }
+
+    if (this.clikOnSaveWithValidFormCount > 1) {
+      // second  time clicked on Save with valid form
+      this.enableShowMessageAlreadySubmitted();
+    }
+  }
 
   onSubmit(submittedForm: any) {
     this.showNewItemCreatedIndexMessage = false;
 
     console.log('form submitted to be save: ' + submittedForm.value);
 
-    if (this.isSavedSuccessfully) {
-      this.enableShowMessageAlreadySubmitted();
-    }
-
     if (submittedForm.invalid) {
+      this.clikOnSaveWithValidFormCount = 0;
       this.validMessage =
         'Please fill out the required fields with valid inputs before submitting the form!';
       this.isSavedSuccessfully = false;
@@ -139,35 +220,41 @@ export class CcpAddOneComponent implements OnInit {
     } else {
       // if submittedForm is valid
 
-      this.readonlyAfterSave = 'readonly';
+      if (this.clikOnSaveWithValidFormCount == 1) {
+        // first time clicked on Save with valid form
 
-      this.showFillForm = false;
+        this.readonlyAfterSave = 'readonly';
 
-      this.firstAttemptToSaveWithValidFormWasDone = true;
+        this.showFillForm = false;
 
-      const ccp: Ccp = {
-        id: uuidv4(),
-        creditCardNumber: submittedForm.value.creditCardNumber,
-        cardHolder: submittedForm.value.cardHolder,
-        expirationDate: submittedForm.value.expirationDate,
-        amount: submittedForm.value.amount,
-        securityCodeCCV: submittedForm.value.securityCodeCCV,
-      };
+        this.firstAttemptToSaveWithValidFormWasDone = true;
 
-      this.ccpSavedFromForm = { ...ccp };
-      this.store.dispatch(createCcp({ ccp }));
-      this.validMessage =
-        'Your new ' + this.itemCapitalizeFullName + ' item has been submitted.';
+        const ccp: Ccp = {
+          id: uuidv4(),
+          creditCardNumber: submittedForm.value.creditCardNumber,
+          cardHolder: submittedForm.value.cardHolder,
+          expirationDate: submittedForm.value.expirationDate,
+          amount: submittedForm.value.amount,
+          securityCodeCCV: submittedForm.value.securityCodeCCV,
+        };
 
-      this.verifyIfIsSavedSuccessfully();
+        this.ccpSavedFromForm = { ...ccp };
+        this.store.dispatch(createCcp({ ccp }));
+        this.validMessage =
+          'Your new ' +
+          this.itemCapitalizeFullName +
+          ' item has been submitted.';
 
-      setTimeout(() => {
-        if (this.isSavedSuccessfully) {
-          this.showNewItemCreatedIndexdIfSavedSuccessfully();
-        } else {
-          this.enableToShowFailureMessageOnSaving();
-        }
-      }, 1200);
+        this.verifyIfIsSavedSuccessfully();
+
+        setTimeout(() => {
+          if (this.isSavedSuccessfully) {
+            this.showNewItemCreatedIndexdIfSavedSuccessfully();
+          } else {
+            this.enableToShowFailureMessageOnSaving();
+          }
+        }, 1200);
+      }
     }
   }
 
@@ -218,7 +305,7 @@ export class CcpAddOneComponent implements OnInit {
 
   showMessageAlreadySubmitted: boolean = false;
 
-  messageAlreadySubmitted: string = 'This item was already saved.';
+  messageAlreadySubmitted: string = 'This item was already submited.';
 
   enableShowMessageAlreadySubmitted() {
     this.showMessageAlreadySubmitted = true;
@@ -226,6 +313,12 @@ export class CcpAddOneComponent implements OnInit {
 
     setTimeout(() => {
       this.showMessageAlreadySubmitted = false;
+      if (this.isSavedSuccessfully) {
+        this.showMessageAlreadySubmitted = false;
+        this.showNewItemCreatedIndexMessage = false;
+        this.firstAttemptToSaveWithValidFormWasDone = false;
+        this.validMessage = 'Your item was submited and successfully saved.';
+      }
     }, 7000);
   }
 
@@ -246,8 +339,6 @@ export class CcpAddOneComponent implements OnInit {
       'view',
     ]);
   }
-
-
 
   goToLastSavedItemEditView() {
     this.getUpdatedIndexFromDatabase();
@@ -280,13 +371,14 @@ export class CcpAddOneComponent implements OnInit {
 
     setTimeout(() => {
       this.showNewItemCreatedIndexMessage = false;
+      this.validMessage = 'Your item was submited and successfully saved.';
     }, 7000);
   }
 
   firstAttemptToSaveWithValidFormWasDone: boolean = false;
 
   messageFailureForFirstAttemptToSave: string =
-    'Failure on saving! The attempt to save the item on server was without success. Unknown cause! Please retry.';
+    'Failure on saving! The attempt to save the item on server was without success. Unknown cause! Please reload the page from browser and retry.';
 
   showFailureMessageOnSaving: boolean = false;
   enableToShowFailureMessageOnSaving() {
@@ -304,5 +396,9 @@ export class CcpAddOneComponent implements OnInit {
 
   delay(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  isNumber(value: string | number): boolean {
+    return value != null && value !== '' && !isNaN(Number(value.toString()));
   }
 }
